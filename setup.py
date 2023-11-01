6c6218d29d33b1dc276346e7db6f34ad5eeda864
Bruno Bento <bruno.bento@delfosim.com>
2023-11-01 12:43:17 -0300

    fix setup

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="mwe_issue_dagster",
-    packages=find_packages(exclude=["mwe_issue_dagster_tests"]),
-    install_requires=[
-        "dagster",
-        "dagster-cloud"
-    ],
-    extras_require={"dev": ["dagster-webserver", "pytest"]},
-)
+setup()
